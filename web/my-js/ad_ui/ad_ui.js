737a5d20badb019a53e5805772897e966405e2fa
wang dehui <bugs_anywhere@sina.com>
2015-07-21 15:23:35 +0800

    2015-07-21

---
+++ web/my-js/ad_ui/ad_ui.js
@@ -0,0 +1,17 @@
+/*
+	affair[i]	. id
+				. a_id
+				. name
+				. step
+				. proposer
+				. processor
+				. content
+				. rec_t
+				. comment[j]. processor
+							. end_t
+							. suggestion
+							. res
+	
+	user_info 	. name
+				. identity
+*/
