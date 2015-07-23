acb6b47c2aa272ff414b515901b0f1ee5813fdae
wang dehui <bugs_anywhere@sina.com>
2015-07-23 18:02:04 +0800

    2015-07-23

--- web/my-js/ad_ui/ad_ui.js
+++ web/my-js/ad_ui/ad_ui.js
@@ -15,3 +15,18 @@
 	user_info 	. name
 				. identity
 */
+
+function add_menu( affair ) {
+	
+	
+}
+
+function menu_click( clicked_id ) {
+	console.log( clicked_id );
+	
+	$('ul.info_list li').show();
+	
+	var tar_str = 'ul.info_list li[class!="'+clicked_id+'"]';
+	$(tar_str).hide();
+	
+}
---
+++ web/my-js/ad_ui/field_affair.js
@@ -0,0 +1,80 @@
+/*
+	
+	场地申请事务处理
+	
+	affair[i]	. id
+				. a_id
+				. name
+				. step
+				. proposer
+				. processor
+				. content
+				. rec_t
+				. group
+				. comment[j]. processor
+							. end_t
+							. suggestion
+							. res
+
+
+*/
+var FIELD_A_ID = 16;
+
+var affair;
+
+affair = [{id:16,group:1},{id:16,group:2},{id:16,group:1},{id:16,group:3},{id:16,group:3},{id:17,g:-1}];
+
+function gen_field_affair_ui( affair ) {
+	var u_id = get_unique( affair );
+	$.each( u_id, function(i, v) {
+		var res = count_with_group( affair, v );
+		
+		if( res.num<=0 )
+			return ture;
+		
+		if( res.num>1 ) {			// 冲突申请
+			gen_field_conflict_apply_ui( affair, res.id );
+			
+		}
+		else {					// 非冲突申请
+			gen_field_normal_apply_ui( affair, res.id );
+
+		};
+		
+	} );
+}
+
+function gen_field_conflict_apply_ui( affair, ids ) {
+	
+	
+}
+
+function gen_field_normal_apply_ui( affair, ids ) {
+	
+	
+}
+
+function get_unique( affair ) {
+	var unique_id = new Array();
+	$.each( affair, function(i, v) {
+		if( 'group' in v && v.id==FIELD_A_ID )
+			unique_id.push( v.group );
+	} );
+	
+	unique_id = $.unique( unique_id );
+	return unique_id;
+}
+
+function count_with_group( affair, g_val ) {
+	var res = { num: 0, id:[] };
+	$.each( affair, function(i, v) {
+		if( 'group' in v  && v.id==FIELD_A_ID ) {
+			if( v.group==g_val ) {
+				res.num++;
+				res.id.push( i );
+			}
+		}
+	} );
+	
+	return res;
+}
