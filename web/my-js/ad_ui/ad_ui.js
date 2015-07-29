60bf41063d9d7a8fe0caa4e4540ffd0f77d8d3ff
wang dehui <bugs_anywhere@sina.com>
2015-07-29 23:42:52 +0800

    2015-07-29

--- web/my-js/ad_ui/ad_ui.js
+++ web/my-js/ad_ui/ad_ui.js
@@ -1,13 +1,15 @@
 /*
-	affair[i]	. id
-				. a_id
-				. name
+	affair[i]	. id					// 此事务唯一标识id
+				. a_id					// 事务类型id
+				. name					// 事务名称
 				. step
 				. proposer
 				. processor
 				. content
-				. rec_t
-				. comment[j]. processor
+				. apply_t				// 申请时间
+				. rec_t					// 此申请进入此步骤的时间
+				. comment[j]. step
+							. processor
 							. end_t
 							. suggestion
 							. res
@@ -29,4 +31,35 @@ function menu_click( clicked_id ) {
 	var tar_str = 'ul.info_list li[class!="'+clicked_id+'"]';
 	$(tar_str).hide();
 	
+}
+
+//-------------------------------------------------------------------------
+//								public functions
+//-------------------------------------------------------------------------
+function parse_content( affair ) {
+	$.each( affair, function(i, v) {
+		if( typeof(v.content)=="undefined" || v.content==='' )
+			return true;
+		affair[i].c_obj = $.parseJSON( v.content );
+	} );
+}
+
+// UTC - 单位为: 秒
+function formatDate( UTC ) {
+	var d = new Date( (UTC+8*3600)*1000 );
+	var year = d.getUTCFullYear();     
+	var month = d.getUTCMonth() + 1;     
+	var date = d.getUTCDate();         
+	return year+"."+month+"."+date;     
+}
+
+function formatDate_2( UTC ) {
+	var d = new Date( (UTC+8*3600)*1000 );
+	var year = d.getUTCFullYear();     
+	var month = d.getUTCMonth() + 1;     
+	var date = d.getUTCDate();     
+	var hour = d.getUTCHours();     
+	var minute = d.getUTCMinutes();     
+	var second = d.getUTCSeconds();     
+	return year+"."+month+"."+date+" "+hour+":"+minute+":"+second;     
 }
--- web/my-js/ad_ui/field_affair.js
+++ web/my-js/ad_ui/field_affair.js
@@ -4,25 +4,45 @@
 	
 	affair[i]	. id
 				. a_id
-				. name
+				. name			
 				. step
 				. proposer
 				. processor
 				. content
+				. apply_t
 				. rec_t
-				. group
-				. comment[j]. processor
+				. group						// 场地申请事务中使用，表明是否为冲突申请
+				. comment[j]. step
+							. processor
 							. end_t
 							. suggestion
 							. res
 
+content - JSON 字符串
 
+		date:	UTC时间戳
+		com:	单位/学校
+		cam:	校区
+		b:		楼
+		room:	房号
+		id:		场地id
+		t:		占用时间, {t1:xx,t2:xx}
+		res:	申请原因
+		
 */
 var FIELD_A_ID = 16;
 
 var affair;
+affair = [{a_id:16,group:1},{a_id:16,group:2},{a_id:16,group:1},{a_id:16,group:3},{a_id:16,group:3},{a_id:17,g:-1}];
+affair[0].content = '{"date":3600,"com":"wdh","cam":"s1","b":"b2","room":"412","t":{"t1":10,"t2":11},"res":"举办一个科学讲座，需要有投影仪，容纳100人以上的场地。"}';
+affair[0].proposer = '匿名者';
+affair[0].id = 'a001';
+affair[0].apply_t = 3600;
 
-affair = [{id:16,group:1},{id:16,group:2},{id:16,group:1},{id:16,group:3},{id:16,group:3},{id:17,g:-1}];
+affair[1].content = '';
+affair[1].proposer = '匿名者2';
+affair[1].id = 'a002';
+affair[1].apply_t = 7200;
 
 function gen_field_affair_ui( affair ) {
 	var u_id = get_unique( affair );
@@ -47,16 +67,46 @@ function gen_field_affair_ui( affair ) {
 function gen_field_conflict_apply_ui( affair, ids ) {
 	
 	var tar = $('ul.info_list');
-	var new_li = $( '<li></li>' );
-	tar.append( new_li );
+	var new_li;
+	var div_l1;
+	var ul;
 	
-	var div_l1 = $( '<div class="f_c"><ul></ul></div>' );
-	new_li.append( div_l1 );
+	$.each( ids, function(i,v) {
+		
+		var cur_a = affair[v];
+		
+		if( i==0 ) {
+			var date = '';
+			var field = '';
+			
+			if( typeof(affair[v].c_obj)!="undefined" ) {
+				var mid = cur_a.c_obj;
+				date = formatDate( mid.date );
+				field = mid.com + '-' + mid.cam + '-' + mid.b + '-' + mid.room;		
+			}
+			
+			new_li = $( '<li><p class="f_h">' + date + '&nbsp;&nbsp;&nbsp;&nbsp;' + field + '</p></li>' );		
+			tar.append( new_li );	
+			
+			div_l1 = $( '<div class="f_c"><ul></ul></div>' );
+			new_li.append( div_l1 );
+			ul = div_l1.children( 'ul' );
+		}
+		
+		var item = $('<li apply="' + cur_a.id + '"><input type="radio" name="a_item"/><div class="field_apply_info"></div></li>');
+		ul.append( item );
+		
+		var apply_info = item.children( 'div[class="field_apply_info"]' );
+		apply_info.append( $('<p class="field_apply_info_p"><b>申请人:</b><a>&nbsp;&nbsp;&nbsp;&nbsp;'+cur_a.proposer+'</a></p>') );
+		
+		apply_info.append( $('<p class="field_apply_info_p"><b>申请使用时段:</b>&nbsp;&nbsp;&nbsp;&nbsp;'+cur_a.c_obj.t.t1+':0:0 - '+(cur_a.c_obj.t.t2+1)+':0:0</p>') );
+		
+		apply_info.append( $('<p class="field_apply_info_p"><b>申请日期:</b>&nbsp;&nbsp;&nbsp;&nbsp;'+formatDate_2(cur_a.apply_t)+'</p>' ) );
+		
+		apply_info.append( $('<p class="field_apply_info_p" style=" line-height:1.5em;width:100%"><b>申请事由:</b><br>'+cur_a.c_obj.res+'</p>') );
+		
+	} );
 	
-	var ul = div_l1.children( 'ul' );
-	ul.append( $('<li><input name="Fruit" type="radio" checked="checked"/>苹果</li>') );
-	ul.append( $("<li><input type='radio' name='w' value='ww'/></li>") );
-
 	
 }
 
@@ -68,7 +118,7 @@ function gen_field_normal_apply_ui( affair, ids ) {
 function get_unique( affair ) {
 	var unique_id = new Array();
 	$.each( affair, function(i, v) {
-		if( 'group' in v && v.id==FIELD_A_ID )
+		if( 'group' in v && v.a_id==FIELD_A_ID )
 			unique_id.push( v.group );
 	} );
 	
@@ -79,7 +129,7 @@ function get_unique( affair ) {
 function count_with_group( affair, g_val ) {
 	var res = { num: 0, id:[] };
 	$.each( affair, function(i, v) {
-		if( 'group' in v  && v.id==FIELD_A_ID ) {
+		if( 'group' in v  && v.a_id==FIELD_A_ID ) {
 			if( v.group==g_val ) {
 				res.num++;
 				res.id.push( i );
