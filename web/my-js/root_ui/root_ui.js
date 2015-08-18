bfa5c8128b15f2063baa34412a7c06743a8a6343
wang dehui <bugs_anywhere@sina.com>
2015-08-19 00:51:10 +0800

    root_ui add srollto

--- web/my-js/root_ui/root_ui.js
+++ web/my-js/root_ui/root_ui.js
@@ -2,50 +2,111 @@
 	user_info 	. uid
 				. identity
 				. name
+				. power[]
 */
 
 user_info = new Array();
 user_info[0] = new Object();
-user_info[0].uid = 45;
+user_info[0].uid = '45';
 user_info[0].identity = 'admin';
 user_info[0].name = '1@163.com';
 
 user_info[1] = new Object();
-user_info[1].uid = 46;
+user_info[1].uid = '46';
 user_info[1].identity = 'admin';
 user_info[1].name = '2@163.com';
 
 user_info[2] = new Object();
-user_info[2].uid = 47;
+user_info[2].uid = '47';
 user_info[2].identity = 'admin';
-user_info[2].name = '1@163.com';
+user_info[2].name = '-1@163.com';
 
 user_info[3] = new Object();
-user_info[3].uid = 48;
+user_info[3].uid = '48';
 user_info[3].identity = 'admin';
 user_info[3].name = '3@163.com';
 
 user_info[4] = new Object();
-user_info[4].uid = 49;
+user_info[4].uid = '49';
 user_info[4].identity = 'admin';
 user_info[4].name = '4@163.com';
 
+user_info[5] = new Object();
+user_info[5].uid = '50';
+user_info[5].identity = 'admin';
+user_info[5].name = '5@163.com';
 
+user_info[6] = new Object();
+user_info[6].uid = '51';
+user_info[6].identity = 'admin';
+user_info[6].name = '6@163.com';
 
-function add_menu( affair ) {
+function list_user( j_obj ) {
+	
+	var tbody = j_obj.children('tbody');
+
+	$.each( user_info, function(i,v) {
+		var tr = $('<tr id="'+v.uid+'"></tr>');
+		tbody.append( tr );
+		tr.append( $('<td>'+v.uid+'</td>') );
+		tr.append( $('<td><a class="user_name">'+v.name+'</a></td>') );
+		tr.append( $('<td><a class="user_identity">'+v.identity+'</a></td>') );
+		tr.append( $('<td><a class="user_power"></a></td>') );
+		tr.append( $('<td class="user_set"><input type="checkbox" class="user_valid"/>禁止<a class="delete">删除</a></td>') );
+	} );
 	
 	
+	$('a.delete').click( function() {
+		var t = $(this).parents('tr');
+		var uid = t.attr('id');
+		
+		$.each( user_info, function(i,v) {
+			if( v.uid==uid ) {
+				user_info.splice( i, 1 );
+				t.hide( 400, function() { $(this).remove(); } );	
+				ii = i;
+				return false;
+			}	
+		} );	
+		
+	} );
+	
+	$('a.icon_search').click( function() {
+		var str = $(this).prev('input[type=search]').val();
+		if( str!='' ) {
+			
+			var uid = -1;
+			
+			// 查uid
+			$.each( user_info, function(i,v) {
+				if( v.uid==str ) {
+					uid = v.uid
+					return false;
+				}
+				
+				if( v.name==str ) {
+					uid = v.uid;
+					return false;
+				}
+				
+				if( v.identity==str ) {
+					uid = v.uid;
+					return false;
+				}
+			
+			} );
+			
+			if( uid>=0 ) {		
+				var y_move = $('#'+uid).offset().top - 80;
+				console.log( y_move );
+				$(window).scrollTop( y_move )
+			}
+			
+		}
+		
+	} );
 }
 
-function menu_click( clicked_id ) {
-	console.log( clicked_id );
-	
-	$('ul.info_list li').show();
-	
-	var tar_str = 'ul.info_list > li[class!="'+clicked_id+'"]';
-	$(tar_str).hide();
-	
-}
 
 //-------------------------------------------------------------------------
 //								public functions
